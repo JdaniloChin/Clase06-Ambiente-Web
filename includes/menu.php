e6afb7e04bc32e71587a1c2ee0dcfa58b6eecb4b
JdaniloChin <danilochin91@gmail.com>
2025-11-01 10:38:44 -0600

    fix

--- includes/menu.php
+++ includes/menu.php
@@ -4,7 +4,7 @@
                 <h4 class="mb-4">Menú</h4>
                 <ul class="nav flex-column">
                     <li class="nav-item"><a class="nav-link text-white" href="./home.php">Inicio</a></li>
-                    <? if(isset($_SESSION['rol']) && $_SESSION['rol'] === "Admin"){
+                    <?php if(isset($_SESSION['rol']) && $_SESSION['rol'] === "Admin"){
                             echo '<li class="nav-item"><a class="nav-link text-white" href="./usuarios.php">Usuarios</a></li>';
                         }
                          ?>
